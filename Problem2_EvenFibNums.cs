8611d50f5af2d9086977f6990936274e0212ad32
Gary Healy <gary.healy2@student.dit.ie>
2017-04-22 21:36:26 +0100

    problem2

---
+++ Problem2_EvenFibNums.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EvenFibonacciNumbers_ProjectEuler_Problem2
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			double gn = Garynacci();
+			Console.WriteLine("The sum of Even valued terms below 4million in fibonnaci sequemce is " + gn);
+		}
+
+		static double Garynacci()
+		{
+			List<double> prev = new List<double>();
+			prev.Add(1);
+			prev.Add(1);
+			int seq = 2;
+			double val;
+			double sumOfEvenValuedTerms = 0;
+				
+			while (sumOfEvenValuedTerms <= 4000000)
+			{
+				val = prev[seq - 1] + prev[seq - 2];
+				prev.Add(val);
+
+				if (isEven(val))
+				{
+					sumOfEvenValuedTerms += val;
+					Console.WriteLine("Seq=" + seq + ", Val= " + val + ", " + prev[seq - 1] + " + " + prev[seq - 2]);
+				}
+
+				seq++;
+			}
+
+			return sumOfEvenValuedTerms;
+		}
+
+		static bool isEven(double val)
+		{
+			return val % 2 == 0;
+		}
+	}
+}
