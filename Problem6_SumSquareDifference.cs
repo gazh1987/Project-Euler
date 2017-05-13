26bdbef6dcb2168d23896d267ef56e75007202d8
Gary Healy <gary.healy2@student.dit.ie>
2017-05-13 23:41:44 +0100

    problem 6

---
+++ Problem6_SumSquareDifference.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace sumAquareDifference_problem6
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int n = 100;
+			Console.WriteLine(sqrOfSum(n) - sumOfSqr(n));
+		}
+
+		static int sumOfSqr(int n)
+		{
+			int s = 0;
+
+			for (int i = 1; i <= n; i++)
+			{
+				s += i* i;
+			}
+
+			return s;
+		}
+
+		static int sqrOfSum(int n)
+		{
+			int s = 0;
+
+			for (int i = 1; i <= n; i++)
+			{
+				s += i;
+			}
+
+			return s * s;
+		}
+	}
+}
